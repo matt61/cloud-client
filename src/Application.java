ef0063a697fe469efaec1ae45479cc9e606d3316
Matt Ward <matt.ward@blackboard.com>
2013-12-10 22:05:31 -0800

    removing raid stuff

--- src/Application.java
+++ src/Application.java
@@ -26,26 +26,6 @@ public class Application {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println(dirs.length);
-		
-		
-        File raid5_disk1 = new File("raid5.disk1");
-        File raid5_disk2 = new File("raid5.disk2");
-        File raid5_disk3 = new File("raid5.disk3");
-        long count = 0;
-        try {
-	        file.RaidStream raid5Stream = new file.RaidStream(new File[]{raid5_disk1, raid5_disk2, raid5_disk3});
-	        FileInputStream in = new FileInputStream("/dev/random");
-	        BufferedInputStream bis = new BufferedInputStream(in);
-	        BufferedOutputStream raid5bos = new BufferedOutputStream(raid5Stream);
-	        while(count < 1024*1024*2){
-	            raid5bos.write(bis.read());
-	            count++;
-	        }
-	        raid5bos.close();
-	        bis.close();
-        } catch (Exception e){
-        	e.printStackTrace();
-        }		
+		System.out.println(dirs.length);	
 	}
 }
